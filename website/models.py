68bc51138a4ac447a30ded7bdc8a1a8ad5636c47
unknown <gabbiematiias@gmail.com>
2019-11-21 22:07:12 -0300

    migracoes

--- website/models.py
+++ website/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Produto(models.Model):
+    opcoes_tamanho = ("pp",'Extra pequeno'),
+    ('P', 'Pequeno'),
+    ('M', 'Medio'),
+    ("G", 'Grande')
+    ("GG",'Extra grande')
+
+
+    nome= models.CharField(max_length=50)
+    preco = models.DecimalField(decimal_places=2, max_digits=1000,default=50)
+    disponivel = models.BooleanField(default=True)
+    quantidade = models.IntegerField(default=1)
+    tamanho = models.CharField(max_length=2,choices=opcoes_tamanho)
+class Pedido(models.Model):
+
+    metodo_pagamento = [
+        ('AV', 'Parcelado em 2x'),
+        ('P2', 'Parcelado em 2x'),
+        ('P3', 'Parcelado em 3x'),
+    ]
+
+    nome = models.CharField(max_length=140)
+    Email = models.EmailField()
+    cartao = models.IntegerField()
+    Pagamento = models.CharField(max_length=2, choices=metodo_pagamento)
+    
